e952f3690987f9b75fcf22a3f3fa1d81f307ef4e
Shailender Singh <geekshailender@gmail.com>
2024-12-13 13:39:07 -0500

    highlight directive

--- form-tutorial/src/app/app.component.ts
+++ form-tutorial/src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 import { ArticleCommentsComponent } from './article-comments/article-comments.component';
+import { HighlightDirective } from './highlight.directive';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,ReactiveFormsModule,ArticleCommentsComponent],
+  imports: [RouterOutlet,ReactiveFormsModule,ArticleCommentsComponent,HighlightDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
