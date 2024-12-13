243a95b8e3fde3e0aeaf3c0f0bc1a7d134edf5d0
Shailender Singh <geekshailender@gmail.com>
2024-12-13 10:46:02 -0500

    defer added

--- form-tutorial/src/app/app.component.ts
+++ form-tutorial/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { ArticleCommentsComponent } from './article-comments/article-comments.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,ReactiveFormsModule],
+  imports: [RouterOutlet,ReactiveFormsModule,ArticleCommentsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ form-tutorial/src/app/article-comments/article-comments.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-article-comments',
+  imports: [],
+  templateUrl: './article-comments.component.html',
+  styleUrl: './article-comments.component.css'
+})
+export class ArticleCommentsComponent {
+
+}
